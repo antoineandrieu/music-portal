3c00d241bb7b6f8fad633d94537df758e6419d2e
Coding Ant <antoine@codingant.io>
2022-01-26 11:46:14 +0100

    feat: customize frontend

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'codingantoine';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -10,9 +10,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🎵 Music Portal</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your music collection in the metaverse ✨
           </p>
         </div>
         <div className="footer-container">
@@ -22,7 +22,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
